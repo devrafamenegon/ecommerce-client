9146d02cda87a14ecfd9b810d36449867f9bc53c
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-06-26 17:28:25 -0300

    feat: create button component

---
+++ components/ui/button.tsx
@@ -0,0 +1,23 @@
+import { forwardRef } from "react";
+
+export interface ButtonProps 
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+  className,
+  children,
+  disabled,
+  type = "button"
+}, ref) => {
+  return (
+    <button
+      ref={ref}
+    >
+      {children}
+    </button>
+  )
+});
+
+Button.displayName = "Button";
+
+export default Button;
