073b0710564a9b411f0870c325a8257c47f2654f
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-06-26 21:52:30 -0300

    feat: add billboard to main page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,18 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard('546840e8-266d-429c-a536-a02f2cf72106');
+
   return (
-    <div>Hello Store</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard}/>
+      </div>
+    </Container>
   )
 };
 
