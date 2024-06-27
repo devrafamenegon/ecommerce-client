739a60dd7b7eb74f8fdf2a643c941bf435adc67d
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-06-27 12:07:34 -0300

    fix: footer margin

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,15 +8,15 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard('546840e8-266d-429c-a536-a02f2cf72106');
+  const billboard = await getBillboard('bc94dbfd-9334-4dfc-81cb-874b1a183eb1');
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard}/>
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   )
