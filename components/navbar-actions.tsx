3df785641b1e6b45c8c30d5fe68d47f351fdc340
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-01 01:11:10 -0300

    feat: trigger add on cart when users click on shopping cart icon in product card

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -4,6 +4,7 @@ import { ShoppingBag } from "lucide-react";
 import { useEffect, useState } from "react";
 
 import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -11,6 +12,8 @@ const NavbarActions = () => {
   useEffect(() => {
     setIsMounted(true);
   }, [])
+
+  const cart = useCart();
 
   if (!isMounted) {
     return null;
@@ -23,7 +26,7 @@ const NavbarActions = () => {
           size={20}
           color="white"
         />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">{cart.items.length}</span>
       </Button>
     </div>
   )
