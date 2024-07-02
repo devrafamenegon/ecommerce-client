553f09692e08f35ad22912cedfd814d705b209f8
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-02 14:15:08 -0300

    feat: add component mount check into cart page to avoid hydration

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
 
@@ -7,7 +9,17 @@ import CartItem from "./components/cart-item";
 import Summary from "./components/summary";
 
 const CartPage = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
   const cart = useCart();
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, []);
+
+  if (!isMounted) {
+    return null
+  };
 
   return (
     <div className="bg-white">
