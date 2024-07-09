07281e5c95be2fce2d22e4c34a9271acad76c7df
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-09 14:45:49 -0300

    feat: add mobile menu to navbar

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -7,8 +7,16 @@ import { useRouter } from "next/navigation";
 import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
 import { SearchBar } from "./ui/search-bar";
+import { Billboard } from "@/types";
+import MobileMenu from "./mobile-menu";
 
-const NavbarActions = () => {
+interface NavbarActionsProps {
+  data: Billboard[]
+};
+
+const NavbarActions: React.FC<NavbarActionsProps> = ({
+  data
+}) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -24,7 +32,10 @@ const NavbarActions = () => {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <SearchBar />
+      <div className="hidden md:flex lg:flex">
+        <SearchBar />
+      </div>
+      
       <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag
           size={20}
@@ -32,6 +43,7 @@ const NavbarActions = () => {
         />
         <span className="ml-2 text-sm font-medium text-white">{cart.items.length}</span>
       </Button>
+      <MobileMenu data={data}/>
     </div>
   )
 }
